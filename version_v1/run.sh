2f01a46fa5a801819fe864efb22bca1647abdb80
Radeox <dawid.weglarz95@gmail.com>
2017-06-25 21:58:19 +0200

    Rimosso menu, ora cerca il sistema in automatico, fix spazi nei nomi

--- version_v1/run.sh
+++ version_v1/run.sh
@@ -8,47 +8,29 @@ PS3='Quale sistema operativo usi? '
 # Funzione di backup
 function backup_android {
     # importa cartelle e file dal dispositivo a .output
-    ./$1/adb shell ls -1 /sdcard/ >.output
+    ./$1/adb shell ls -1 -a /sdcard/ > .output
 
-    # Sostituisce nei nomi 'a b' con 'a\ b'
-    cat .output | awk '{gsub(/ /,"\\ ")}8' > .cache
-
-    # Elimina i residui di ADB, .output, e ricrea il file aggiornato
-    sed -i -e '1d;2d' .cache && rm .output && mv .cache .output
+    # Elimina i residui di ADB
+    sed -i '1d;2d' .output
 
     # Crea cartella backup
     mkdir backup_android_`date "+%d-%m-%Y"`/$p 2>/dev/null
 
     # Legge il file .output e usa ADB per fare il backup
-    while read -rs p; do
-        ./$1/adb pull "/mnt/sdcard/$p" $a""backup_android_`date "+%d-%m-%Y"`/$p
+    while read p; do
+        ./$1/adb pull "/mnt/sdcard/$p" "backup_android_`date "+%d-%m-%Y"`/$p"
     done < .output
 }
 
 ## Main
-
-# Pulizia schermo
 clear && reset
-echo "Backup Android"
+echo Avvio Backup Android...
 
-select opt in "${options[@]}"
-do
-    case $opt in
-        "Linux")
-            clear && reset
-            echo "Avvio Backup..."
-            backup_android 'linux'
-            break
-            ;;
-        "MacOS")
-            clear && reset
-            echo "Avvio Backup..."
-            backup_android 'macos'
-            break
-            ;;
-        "Esci")
-            break
-            ;;
-        *) echo invalid option;;
-    esac
-done
+# Controllo sistema
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    # Linux
+    backup_android 'linux'
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    # Mac OSX
+    backup_android 'macos'
+fi
