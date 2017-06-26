a11defdbc454bc8509b5b5c5b62fbae03a39e0b4
Radeox <dawid.weglarz95@gmail.com>
2017-06-26 13:10:47 +0200

    Fix rimozione e aggiunto tasto 'backup'

--- version_v2/run.sh
+++ version_v2/run.sh
@@ -4,76 +4,73 @@
 options=("Linux" "MacOS" "Esci")
 PS3='Quale sistema operativo usi? '
 
+
 # Funzione di backup
 function backup_android {
     # Importa cartelle e file dal dispositivo a .output
-    ./$1/adb shell ls -1 -a /sdcard/ > .output
-
-    # Elimina i residui di ADB
-    sed -i -e '1d;2d' .output
+    ./$1/adb shell ls -1 -A /sdcard/ > .output
 
     # Crea cartella backup
     mkdir backup_android_`date "+%d-%m-%Y"`/$p 2>/dev/null
-
-function main (){
-s2=()
-#numero di line dentro il file .output
-cnt=$(wc -l < .output)
-j=0
-for ((i=1;i<cnt+1;i++)); do
-    s2[j]=`expr $i + 0`
-    j=`expr $j + 1`
-    s2[j]=$(sed -n $i'p' < .output)
-    j=`expr $j + 1` 
-done
-while true 
-do
-OPTION=$(dialog --title "Backup Android" --menu "Seleziona i file/cartelle da escludere dal backup \n\n(Per confermare il backup clicca su Cancel)"  80 60 15 "${s2[@]}" 3>&1 1>&2 2>&3)
-exitstatus=$?
-if [ $exitstatus = 0 ]; then
-    #elimina i nomi selezionati
-    sed -i -e $OPTION'd'  .output
-    main
-else
-
-    # Legge il file .output e usa ADB per fare il backup
-    while read p; do
-        ./$1/adb pull "/mnt/sdcard/$p" "backup_android_`date "+%d-%m-%Y"`/$p"
-    done < .output
-    exit
-fi
-done
 }
+
+# Menu principale
+function display_menu {
+    while true; do
+        # Costruisco le entry del menu
+        i=1
+        s2=''
+
+        for entry in `cat .output`; do
+            s2+="$i $entry "
+            let i+=1
+        done
+
+        # Display Menu
+        OPTION=$(dialog --title "Backup Android" --extra-button --extra-label "Backup"\
+                 --menu "Seleziona i file/cartelle da escludere dal backup" 80 60 15 $s2 3>&1 1>&2 2>&3)
+
+        # Scelta utente
+        rv=$?
+
+        if [ $rv == 3 ]; then
+            clear
+
+            # Legge il file .output e usa ADB per fare il backup
+            while read p; do
+                ./$1/adb pull "/mnt/sdcard/$p" "backup_android_`date "+%d-%m-%Y"`/$p"
+            done < .output
+            exit
+        elif [ $rv == 0 ]; then
+            # Elimina il nome selezionato
+            sed -i -e $OPTION'd' .output
+        else
+            exit
+        fi
+    done
 }
+
+
 ## Main
-clear && reset
-echo Avvio Backup Android
-echo --------------------
-
 # Controllo sistema
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
+if [ $OSTYPE == "linux-gnu" ]; then
     # Linux
     MACHINE_TYPE=`uname -m`
 
     if [ ${MACHINE_TYPE} == 'x86_64' ]; then
         # 64-bit
-        brew install dialog
         backup_android 'linux'
-        main 'linux'
-        main
+        display_menu 'linux'
     else
         # 32-bit
-        brew install dialog
         backup_android 'linux32'
-        main 'linux32'
-        main
+        display_menu 'linux32'
     fi
-elif [[ "$OSTYPE" == "darwin"* ]]; then
+elif [ $OSTYPE == "darwin"* ]; then
     # Mac OSX
     brew install dialog
     backup_android 'macos'
-    main 'macos'
-    main
+    display_menu 'macos'
 else
     echo Sistema non riconosciuto...
 fi
